02a72c8c6041545777435a8448d198e2583ea87d
hyeonyi <hyeonyi@github.com>
2015-08-18 16:12:05 +0900

    update Adlibr sample project

---
+++ adlibrTestProject/src/test/adlib/project/AdlibTestProjectConstans.java
@@ -0,0 +1,11 @@
+package test.adlib.project;
+
+public class AdlibTestProjectConstans {
+	// 애드립 광고를 테스트 하기 위한 키 입니다.
+//	public static String ADLIB_API_KEY = "559492b70cf222dbc1732120"; //현이-애드립테스트
+	public static String ADLIB_API_KEY = "55012a550cf2036b35383c28"; //선정-테스트
+	
+	// false : 상용 광고, true : 테스트 광고
+	public static boolean ADLIB_TEST_MODE = false;
+
+}
